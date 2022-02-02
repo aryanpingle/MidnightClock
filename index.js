d5f158c3478841e6240c9a4558bd3370090ad5be
robo-mop <realaryanpingle@gmail.com>
2022-02-03 01:32:35 +0530

    IDK

--- index.js
+++ index.js
@@ -7,20 +7,44 @@ const min = (iterable, key = e=>e) => iterable.reduce((acc, val) => key(val) <= 
 const create_html = (html_string) => new DOMParser().parseFromString(html_string, "text/html").body.firstElementChild
 
 let path_element = null
+const countdown_arc_width = 75
 
 function setup() {
-    path = document.querySelector("#countdown-path")
+    document.querySelectorAll(".countdown-element").forEach((countdown_element, index) => {
+        // Set path lengths
+        countdown_element.style.setProperty("--path-length", countdown_element.getAttribute("pathLength"))
+        countdown_element.querySelectorAll("circle").forEach(circle => {
+            circle.setAttribute("pathLength", countdown_element.getAttribute("pathLength"))
+            
+            // Set radius
+            // [0] = 1000 - width/2
+            // [1] = 1000 - 3*width/2
+            // ...
+            // [i] = 1000 - width*(1+2i)/2
+            circle.setAttribute("r", 500 - countdown_arc_width*(1 + 2*index)/2)
+        })
+    })
 }
 
 function tick() {
     let date = new Date()
-    let seconds_left = (date.getMinutes() * 60 + date.getSeconds());
-    path.style.setProperty("--time-elapsed", seconds_left)
+
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+
+    // Set Seconds Left
+    document.querySelector("#seconds-left-group").style.setProperty("--time-elapsed", seconds)
+
+    // Set Minutes Left
+    document.querySelector("#minutes-left-group").style.setProperty("--time-elapsed", seconds + minutes*60)
+
+    // Set Hours Left
+    document.querySelector("#hours-left-group").style.setProperty("--time-elapsed", seconds + minutes*60 + hours*3600)
 
     // Text - Time Left
-    document.getElementById("time").innerHTML = `<span>${24 - date.getHours() - 1}</span>:<span>${60 - date.getMinutes() - 1}</span>:<span>${60 - date.getSeconds()}</span>`
+    document.getElementById("time-as-text").innerHTML = `<span>${24 - date.getHours() - 1}</span>:<span>${60 - date.getMinutes() - 1}</span>:<span>${(60 - date.getSeconds())}</span>`
 }
 
 setup()
-
 setInterval(() => tick(), 500)

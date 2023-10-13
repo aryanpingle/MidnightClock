8a63119a0970099f74d149ea1610e7068b4690eb
robo-mop <realaryanpingle@gmail.com>
2023-10-13 23:35:39 +0530

    Switch to start and target model

--- index.js
+++ index.js
@@ -12,6 +12,70 @@ let seconds_left_group = null
 let minutes_left_group = null
 let hours_left_group = null
 
+class DateRepresentation {
+    secondsOTD;
+    minutesOTD;
+    hoursOTD;
+    /**
+     * 1:30AM = 013000
+     * 1:31AM = 013100
+     * 1:30PM = 133000
+     */
+    dateMask;
+
+    /**
+     * 1:31AM - 1:30AM = 000100
+     * 2:30AM - 1:40AM = 005000
+     */
+
+    constructor(arg, isDateMask=0) {
+        if(isDateMask) {
+            this.constructorFromDateMask(arg);
+        }
+        else {
+            this.constructorFromDate(arg);
+        }
+    }
+
+    constructorFromDate(date) {
+        this.secondsOTD = date.getSeconds();
+        this.minutesOTD = date.getMinutes();
+        this.hoursOTD = date.getHours();
+        this.dateMask = (this.hoursOTD * 10000) + (this.minutesOTD * 100) + this.secondsOTD;
+    }
+
+    constructorFromDateMask(dateMask) {
+        this.secondsOTD = dateMask % 1_00;
+        if(this.secondsOTD >= 60) {
+            this.secondsOTD = 100 - this.secondsOTD;
+        }
+        this.minutesOTD = parseInt((dateMask % 1_00_00) / 1_00);
+        if(this.minutesOTD >= 60) {
+            this.minutesOTD = 100 - this.minutesOTD;
+        }
+        this.hoursOTD = parseInt(dateMask / 1_00_00);
+        this.dateMask = (this.hoursOTD * 10000) + (this.minutesOTD * 100) + this.secondsOTD;
+    }
+
+    /**
+     * Shifts the current DateRepresentation object so that `newDateRepr` is midnight relative to the current object
+     * @param {DateRepresentation} newDateRepr 
+     */
+    static getDifferenceDateRepresentation(startDateRepr, targetDateRepr) {
+        let dateMaskStart = targetDateRepr.dateMask;
+        let dateMaskEnd = startDateRepr.dateMask;
+        if (dateMaskEnd < dateMaskStart) {
+            // Add 24 hours to end time
+            dateMaskEnd += 24_00_00;
+        }
+        return new DateRepresentation(dateMaskEnd - dateMaskStart, true);
+    }
+
+    toString() {
+        return `Date: ${this.dateMask}`;
+    }
+}
+
 function setup() {
     // Set theme
     if(window.matchMedia("(prefers-color-scheme: light)").matches) {
@@ -47,34 +111,28 @@ function tick() {
     const arcUnitMinutes = parseInt(DIVISIONS / 60 / 60);
     const arcUnitHours = parseInt(DIVISIONS / 60 / 60 / 24);
 
-    let date = new Date()
+    const currentDateRepr = new DateRepresentation(new Date());
+    const targetDateRepr = new DateRepresentation(240000, true); // Midnight by default
+    const elapsedDateRepr = DateRepresentation.getDifferenceDateRepresentation(currentDateRepr, targetDateRepr);
+    console.log(`${elapsedDateRepr}`);
 
-    let hours = date.getHours()
-    let minutes = date.getMinutes()
-    let seconds = date.getSeconds()
+    const elapsedSeconds = elapsedDateRepr.secondsOTD;
+    const elapsedMinutes = elapsedDateRepr.minutesOTD;
+    const elapsedHours = elapsedDateRepr.hoursOTD;
 
     // Set Seconds Left
-    seconds_left_group.style.setProperty("--time-elapsed", seconds * arcUnitSeconds)
+    seconds_left_group.style.setProperty("--time-elapsed", elapsedSeconds * arcUnitSeconds)
 
     // Set Minutes Left
-    minutes_left_group.style.setProperty("--time-elapsed", (seconds + minutes*60) * arcUnitMinutes)
+    minutes_left_group.style.setProperty("--time-elapsed", (elapsedSeconds + elapsedMinutes*60) * arcUnitMinutes)
 
     // Set Hours Left
-    hours_left_group.style.setProperty("--time-elapsed", (seconds + minutes*60 + hours*3600) * arcUnitHours);
+    hours_left_group.style.setProperty("--time-elapsed", (elapsedSeconds + elapsedMinutes*60 + elapsedHours*3600) * arcUnitHours);
 
     // Text - Time Left
-    let hours_left = 24 - date.getHours() - 1
-    let minutes_left = 60 - date.getMinutes() - 1
-    let seconds_left = 60 - date.getSeconds()
-
-    if (seconds_left == 60) {
-        seconds_left = 0
-        ++minutes_left
-    }
-    if(minutes_left == 60) {
-        minutes_left = 0
-        ++hours_left
-    }
+    let hours_left = 23 - elapsedHours
+    let minutes_left = 59 - elapsedMinutes
+    let seconds_left = 59 - elapsedSeconds
 
     document.getElementById("time-as-text").innerHTML = `
     ${getTimeSubdivision(hours_left, "hours")}
